a766527733f0c4c9db85644be194d7afdf0ce130
Janson <jansonlui@berkeley.edu>
2016-08-05 00:49:53 -0700

    version 2

--- wordcount.clj
+++ wordcount.clj
@@ -1,28 +1,22 @@
-(ns tweetwordcount
+(ns wordcount
   (:use     [streamparse.specs])
   (:gen-class))
 
-(defn tweetwordcount [options]
+(defn wordcount [options]
    [
     ;; spout configuration
     {"tweet-spout" (python-spout-spec
           options
           "spouts.tweets.Tweets"
           ["tweet"]
-          :p 3
+          :p 2
           )
     }
+
     ;; bolt configuration
-    {"parse-tweet-bolt" (python-bolt-spec
+    {"count-bolt" (python-bolt-spec
           options
           {"tweet-spout" :shuffle}
-          "bolts.parse.ParseTweet"
-          ["word"]
-          :p 3
-          )
-     "count-bolt" (python-bolt-spec
-          options
-          {"parse-tweet-bolt" ["word"]}
           "bolts.wordcount.WordCounter"
           ["word" "count"]
           :p 2
