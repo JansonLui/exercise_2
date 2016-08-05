a30fb067bfc8e71a5f2049a39b2eefc043026187
Janson <jansonlui@berkeley.edu>
2016-08-05 01:32:00 -0700

    Version 5

--- src/bolts/wordcount.py
+++ src/bolts/wordcount.py
@@ -3,13 +3,11 @@ from __future__ import absolute_import, print_function, unicode_literals
 from collections import Counter
 from streamparse.bolt import Bolt
 import psycopg2
-#from redis import StrictRedis
 
 class WordCounter(Bolt):
 
     def initialize(self, conf, ctx):
         self.counts = Counter()
-#        self.redis = StrictRedis()
 
     def process(self, tup):
         word = tup.values[0]
